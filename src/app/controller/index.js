832ebdf3240f5708c073bd2c866abcb8d20fa07e
chicofariasneto <chicofariasneto@gmail.com>
2020-12-16 23:49:02 -0300

    style(src/index): add documentation and ident code (auto)

--- src/app/controller/index.js
+++ src/app/controller/index.js
@@ -1,10 +1,13 @@
+/**
+ * Imports
+ */
 const fs = require('fs');
 const path = require('path');
 
 /**
  * fs allows working with the node's file system path works with folder / directory paths
- * .readdirSysnc he reads a directory __dirname is this directory I'm working on\
- * .filter is used to ignore files that start with. and the index.js file, which in this case is this\
+ * .readdirSysnc he reads a directory __dirname is this directory I'm working on
+ * .filter is used to ignore files that start with. and the index.js file, which in this case is this
  * .forEach to require each file and pass the app to each one and with that each new
  * controller created is automatically added to the project
  */
