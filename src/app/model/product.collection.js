fe54c84c2e6c670c600e23d399223458c0ea8958
chicofariasneto <chicofariasneto@gmail.com>
2020-12-16 23:05:32 -0300

    refactor(product.collection): fix and edit collection mongodb that save an order

--- src/app/model/product.collection.js
+++ src/app/model/product.collection.js
@@ -1,1 +1,25 @@
-// TODO create collection to be used to create products from PipeDrive
+/**
+ * Imports mongoose
+ */
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+/**
+ * Schema do create an order for save deals from pipedrive
+ */
+const schema = new Schema({
+    _id: {type: Number}, /** I could use Schema.ObjectId but I'm gonna use id from Pipedrive */
+    cliente: {type: Object},
+    transporte: {type: Object},
+    itens: {type: []},
+    parcelas: {type: []},
+    vlr_frete: {type: Number},
+    vlr_desconto: {type: Number},
+    obs: {type: String},
+    obs_internas: {type: String}
+});
+const Order = mongoose.model('order', schema);
+
+module.exports = {
+    Order
+}
