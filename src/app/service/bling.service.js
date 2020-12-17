a53c2246e6a72d51d5ce2c06819edf56f74a410a
chicofariasneto <chicofariasneto@gmail.com>
2020-12-16 23:07:25 -0300

    refactor(bling.service): change the out of sendOrder function

--- src/app/service/bling.service.js
+++ src/app/service/bling.service.js
@@ -14,19 +14,19 @@ const bling = {
  * This function send a order to Bling
  *
  * @param xmlData - Order in a XML String
- * @returns {Promise<{data: {}, success: boolean}>}
+ * @returns {Promise<{success: boolean}>}
  */
 const sendOrder = async (xmlData) => {
     try {
         /** I have tried pass info and data using params object like: "params: {}", but for some reason
          * it didn't work, so i put the params directly in url, it's not fancy ): , but it works (:
          */
-        const response = await axios.post(`${bling.url}pedido/json/apikey=${bling.apikey}&xml=${xmlData}`);
+        await axios.post(`${bling.url}pedido/json/?apikey=${bling.apikey}&xml=${xmlData}`);
 
-        return {success: true, data: response};
+        return {success: true};
     } catch (error) {
         console.log(error);
-        return {success: false, data: {}};
+        return {success: false};
     }
 }
 
